f4763c3cc2c5643aeacdff660272a3f335178241
Thiago Yugue Kono Campos <106500654+kono100@users.noreply.github.com>
2023-02-19 02:38:08 -0300

    Add files via upload

---
+++ 21ex  PAR ou IMPAR.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+#21. Elabore um programa que leia um valor inteiro e informe se ele é PAR ou ÍMPAR.
+
+numero = int(input("Digite um numero : "))
+
+if (numero % 2 ==0):
+    print(f"\nO número digitado {numero}, é PAR!\n")
+else:
+    print(f"\nO número digitado {numero}, é IMPAR!\n")
